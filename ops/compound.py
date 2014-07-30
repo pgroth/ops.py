f97f077af57263f3368a4bcdb7b05d7627700689
Paul Groth <pgroth@gmail.com>
2014-07-30 14:25:09 +0200

    updated to instantiate data from the api through the constructor. The compound class now has methods for returning pharmacologically associated targets

--- ops/compound.py
+++ ops/compound.py
@@ -1,9 +1,62 @@
+from target import *
+import urllib2
+
 class Compound:
-  def __init__(self, api, compound_info_results):
+  def __init__(self, api, compound_uri):
       self.api = api
-      self._parse_and_populate(compound_info_results)
+      self.init_uri = compound_uri
+      res = api.getCompoundInfo(compound_uri)
+      self._parse_and_populate(res)
+      self.json = res
+
+
+  def get_pharmacological_protein_targets(self):
+    for i in self.api.getCompoundPharmacology("http://www.conceptwiki.org/concept/dd85c868-74be-4b86-ae58-12ac6d19a4ba"):
+      if "hasAssay" in i:
+        if "hasTarget" in i['hasAssay']:
+          if type(i['hasAssay']['hasTarget']) is dict:
+            tar = i['hasAssay']['hasTarget']
+            if tar['type'] == "http://rdf.ebi.ac.uk/terms/chembl#SingleProtein":
+              try:
+                tar = Target(self.api, tar['_about'])
+                yield tar
+              except urllib2.HTTPError:
+                #print "No Information available for:" + i['hasAssay']['hasTarget']['_about']
+                pass
+          else:
+            for t in i['hasAssay']['hasTarget']:
+              if t['type'] == "http://rdf.ebi.ac.uk/terms/chembl#SingleProtein":
+                try:
+                  tar = Target(self.api, t['_about'])
+                  yield tar
+                except urllib2.HTTPError:
+                  #print "No Information available for:" + t['_about']
+                  pass
+
+  def get_pharmacological_targets(self):
+
+     for i in self.api.getCompoundPharmacology("http://www.conceptwiki.org/concept/dd85c868-74be-4b86-ae58-12ac6d19a4ba"):
+        if "hasAssay" in i:
+          if "hasTarget" in i['hasAssay']:
+            if type(i['hasAssay']['hasTarget']) is dict:
+              try:
+                tar = Target(self.api, i['hasAssay']['hasTarget']['_about'])
+                yield tar
+              except urllib2.HTTPError:
+                #print "No Information available for:" + i['hasAssay']['hasTarget']['_about']
+                pass
+            else:
+              for t in i['hasAssay']['hasTarget']:
+                try:
+                   tar = Target(self.api, t['_about'])
+                   yield tar
+                except urllib2.HTTPError:
+                  #print "No Information available for:" + t['_about']
+                  pass
+
 
   def _parse_and_populate(self, compound_info_results):
+
 
     primaryTopic = compound_info_results['result']['primaryTopic']
     if "prefLabel" in primaryTopic:
--- ops/target.py
+++ ops/target.py
@@ -1,12 +1,20 @@
 class Target:
-  def __init__(self, api, compound_info_results):
+  def __init__(self, api, target_uri):
       self.api = api
-      self._parse_and_populate(compound_info_results)
+      self.init_uri = target_uri
+      res = api.getTargetInfo(target_uri)
+      self._parse_and_populate(res)
+      self.json = res
 
   def _parse_and_populate(self, compound_info_results):
     primaryTopic = compound_info_results['result']['primaryTopic']
     if "prefLabel" in primaryTopic:
       self.label = primaryTopic['prefLabel']
+    elif "label" in primaryTopic:
+      self.label = primaryTopic['label']
+
+    if "type" in primaryTopic:
+      self.type = primaryTopic['type']
 
     exactMatches = primaryTopic['exactMatch']
 
