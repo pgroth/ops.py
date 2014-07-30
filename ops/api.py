2c22d15d9dedfdb2aad9f4855ca9dbfc747bfe40
Paul Groth <pgroth@gmail.com>
2014-07-30 14:24:00 +0200

    added functions for target pharmacology

--- ops/api.py
+++ ops/api.py
@@ -1,6 +1,8 @@
 import json
 import urllib2
 import urllib
+from compound import *
+from target import *
 
 
 class API:
@@ -8,7 +10,7 @@ class API:
   def __init__(self, app_id, app_key):
     self.app_id = app_id
     self.app_key = app_key
-    self.base_url = "https://beta.openphacts.org/1.3/"
+    self.base_url = "https://beta.openphacts.org/1.4/"
 
 
   def getCompoundInfo(self, uri):
@@ -19,12 +21,14 @@ class API:
     results = json.load(urllib2.urlopen(url_str))
     return results
 
+
   def getTargetInfo(self, uri):
     uri_encoded = urllib.quote_plus(uri)
     url_str = self.base_url+ "target?app_id=" + self.app_id + "&app_key=" + self.app_key + "&uri="+ uri_encoded + "&_format=json"
     #print url_str
     results = json.load(urllib2.urlopen(url_str))
     return results
+
 
   def getCompoundPharmacology(self, uri):
      uri_encoded = urllib.quote_plus(uri)
@@ -43,4 +47,23 @@ class API:
      if "next" in results['result']:
         url_str_next = results['result']['next']
         for x in self.getCompoundPharmacologyIter(url_str_next):
+          yield x
+
+  def getTargetPharmacology(self, uri):
+     uri_encoded = urllib.quote_plus(uri)
+     page_num = "1"
+     url_str = self.base_url+ "target/pharmacology/pages?app_id=" + self.app_id + "&app_key=" + self.app_key + "&uri="+ uri_encoded + "&_page="+ page_num +"&_format=json"
+     for x in self.getCompoundPharmacologyIter(url_str):
+       yield x
+
+  def getTargetPharmacologyIter(self, url_str):
+     results = json.load(urllib2.urlopen(url_str))
+     items = results['result']['items']
+
+     for item in items:
+          yield item
+
+     if "next" in results['result']:
+        url_str_next = results['result']['next']
+        for x in self.getTargetPharmacology(url_str_next):
           yield x
